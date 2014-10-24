00023edd202426c9bbb9df78614cb4459a250334
AradhanaR <aradhana.online19@gmail.com>
2014-10-24 20:21:49 +0530

    4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  attr_accessor :login
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :user_name,
-                  :first_name,
-                  :last_name
+  
 end

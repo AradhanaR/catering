a662afcc77dc9b85356b3f5061e997ed3e1fe3ea
AradhanaR <aradhana.online19@gmail.com>
2014-10-25 21:04:43 +0530

    464

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,11 @@
-class UserController < ApplicationController
+class UserController  < ApplicationController
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_name)
+  end
 end
